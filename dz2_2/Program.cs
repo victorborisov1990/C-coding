4dd21c1cfaf3ac9fb311b5654145a7618b103619
victor <victorborisov1990@gmail.com>
2022-12-03 15:36:43 +0300

    Добавлено домашнее задание 2

---
+++ dz2_2/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Clear();
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+if (number < 100)
+     Console.Write("Нет 3й цифры");   
+else 
+    {
+    while (number > 999) //пока в числе больше 3 цифр, будем делить его на 10
+         number = number / 10;   
+    Console.WriteLine($"Третья цифра числа: {number%10}");//вывод 3 цифры в оставшимся 3значном числе
+    }
