113dbc22dab3fb0a00db12948ac5df56c89861cc
victor <victorborisov1990@gmail.com>
2022-12-12 12:13:38 +0300

    Домашнее задание 4

---
+++ dz4/dz4_1/Program.cs
@@ -0,0 +1,19 @@
+﻿//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.Clear();
+Console.Write("Введите число, которое нужно возвести в степень: ");
+int number = int.Parse(Console.ReadLine());
+Console.Write("Введите степень: ");
+int stepen = int.Parse(Console.ReadLine());
+int result = number;
+int step_show = stepen;//для красивого вывода
+while (stepen < 0) //если введена отрицательная степень, вводим, пока не введут положительную
+    {
+        Console.Write("Введите степень больше 0: ");
+        stepen = int.Parse(Console.ReadLine());   
+    }
+while (stepen!=1)//таким образом, если степень 1, то умножения не будет, иначе возводить будет нужное количество раз
+{
+    result=result*number;
+    stepen--;
+}
+Console.WriteLine($"{number}^{step_show} = {result}");
---
+++ dz4/dz4_2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string number = Console.ReadLine();//получаем число как строку
+double sum = 0;
+for (int i = 0; i < number.Length; i++)//перебираем сроку посимвольно
+    sum+=Char.GetNumericValue(number[i]);//каждый символ преобразуем в число и прибавляем к сумме
+Console.WriteLine($"Сумма = {sum}");
