c769968a842ed2203b79833dc74e46b9075458db
victor <victorborisov1990@gmail.com>
2022-12-30 09:45:11 +0300

    Домашнее задание 9

---
+++ dz9/dz9_1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте начальное значение n и конечное m. вывести на экран сумму чисел от n до m. Через рекурсию
+
+int Sum (int start, int end)
+{
+    if(start==end)
+        return end;
+    return Sum(start+1,end)+start;
+}
+
+Console.Clear();
+Console.Write("Введите начальное значение последовательности: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите конечное значение последовательности: ");
+int m = Convert.ToInt32(Console.ReadLine());
+while (n>=m)
+{
+    Console.Write("Введите начальное значение последовательности: ");
+    n = Convert.ToInt32(Console.ReadLine());
+    Console.Write("Введите конечное значение последовательности: ");
+    m = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(Sum(n,m));
+
---
+++ dz9/dz9_2/Program.cs
@@ -0,0 +1,28 @@
+﻿// Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n
+
+int Akkerman (int m, int n)
+{
+    if(m == 0)
+        return n+1;
+    if(n == 0)
+        return Akkerman(m-1,1);
+    return Akkerman(m-1, Akkerman(m, n-1));
+}
+
+Console.Clear();
+Console.WriteLine("Необходимо ввести аргументы функции Аккермана");
+Console.Write("Введите аргумент m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+while(m < 0)
+{
+    Console.Write("Число не должно быть отрицательным. Введите аргумент m: ");
+    m = Convert.ToInt32(Console.ReadLine());
+}
+Console.Write("Введите аргумент n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n < 0)
+{
+    Console.Write("Число не должно быть отрицательным. Введите аргумент n: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine(Akkerman(m,n));
